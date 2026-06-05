00588205fb0b59b869d6e4257f36dbb6453de642
MONEYWAY-maker <lenwoodsbiz@gmail.com>
2026-06-05 23:05:16 +0000

    find min length

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
+function findMinLength(word1, word2)
+{    let minLength = Math.min(word1.length, word2.length);
+    return minLength;
+}
 
+console.log(findMinLength("hlo", "worjcjsbcld")); // Output: 5
